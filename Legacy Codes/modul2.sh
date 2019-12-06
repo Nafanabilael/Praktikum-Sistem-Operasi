bfccfc957477d9c3fc3d5a968ff0103b221ec620
Nafanabilael <54533295+Nafanabilael@users.noreply.github.com>
2019-12-06 13:16:35 +0700

    Add files via upload

---
+++ Legacy Codes/modul2.sh
@@ -0,0 +1,20 @@
+echo "========================";
+echo "|Program Konversi Waktu|";
+echo "========================";
+
+echo "Masukan menit : ";
+read menit;
+let hari=$menit/1440
+let sisa=$menit%1440
+let jam=$sisa/60
+let sisa=$sisa%60
+ if [ $menit -gt 1440 ]
+then
+    echo -e "Hari	: $hari\nJam	: $jam\nMenit	: $sisa "
+elif [ $menit -gt 60 ]
+then
+    echo -e "Jam	: $jam\nMenit	: $sisa "
+elif [ $menit -lt 60 ]
+then
+    echo    "Menit	: $sisa"
+fi
